f47abb650fd324c2ae6ec13838aa8f0fdc939bff
Grace02505 <nyokabinjau99@gmail.com>
2023-06-11 23:43:27 +0300

    wors.py

--- conditionals.py
+++ conditionals.py
@@ -1,7 +1,11 @@
-age= 24
-if age <= 18:
-    print('You cant vote')
-if age >= 18:
-    print('Youre about to vote')
-else:
-    print('you can vote')
+#age= 24
+#if age <= 18:
+#  print('You cant vote')
+#if age >= 18:
+#    print('Youre about to vote')
+#else:
+#    print('you can vote')
+
+
+
+
